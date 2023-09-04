63fed5396c1979ba41850887b0583511872cc33d
WataruNishimura <wataru.nishimura@nnkk.work>
2023-09-04 16:16:34 +0900

    initial

---
+++ app/models/concerns/uuid_module.rb
@@ -0,0 +1,16 @@
+require "active_support/concern"
+
+module UuidModule
+  extend ActiveSupport::Concern
+
+  included do 
+    before_create :generate, if: -> {has_attribute?(:id)}
+  end
+
+  def generate 
+    self.id ||= loop do
+      uuid = SecureRandom.uuid
+      break uuid unless self.class.exists?(id: uuid)
+    end
+  end
+end
